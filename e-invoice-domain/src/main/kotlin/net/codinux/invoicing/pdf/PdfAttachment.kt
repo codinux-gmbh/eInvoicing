519c53aef009d004423071a4913420b3a9ca4f1c
dankito <git@dankito.net>
2024-12-15 19:31:33 +0100

    Extracted PdfBoxPdfAttachmentReader

--- e-invoice-domain/src/main/kotlin/net/codinux/invoicing/pdf/PdfAttachment.kt
+++ e-invoice-domain/src/main/kotlin/net/codinux/invoicing/pdf/PdfAttachment.kt
@@ -3,7 +3,8 @@ package net.codinux.invoicing.pdf
 data class PdfAttachment(
     val filename: String,
     val isXmlFile: Boolean,
+    val isProbablyEN16931InvoiceXml: Boolean,
     val xml: String?
 ) {
-    override fun toString() = "$filename (isXml? $isXmlFile)"
+    override fun toString() = "$filename (isInvoiceXml? $isProbablyEN16931InvoiceXml)"
 }
