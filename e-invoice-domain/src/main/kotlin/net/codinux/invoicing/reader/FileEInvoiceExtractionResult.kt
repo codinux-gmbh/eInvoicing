6958f4cd5a2550b8a52626bcf701add80560733c
dankito <git@dankito.net>
2024-12-15 20:21:16 +0100

    Added path property

--- e-invoice-domain/src/main/kotlin/net/codinux/invoicing/reader/FileEInvoiceExtractionResult.kt
+++ e-invoice-domain/src/main/kotlin/net/codinux/invoicing/reader/FileEInvoiceExtractionResult.kt
@@ -2,6 +2,8 @@ package net.codinux.invoicing.reader
 
 import net.codinux.invoicing.model.Invoice
 import net.codinux.invoicing.pdf.PdfEInvoiceExtractionResult
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
 
 data class FileEInvoiceExtractionResult(
     val filename: String,
@@ -10,6 +12,8 @@ data class FileEInvoiceExtractionResult(
     val xml: ReadEInvoiceXmlResult?
 ) {
     val invoice: Invoice? = pdf?.invoice ?: xml?.invoice
+
+    val path: String by lazy { if (directory != null) Path(directory).resolve(filename).absolutePathString() else filename }
 
     override fun toString() = "$filename ${pdf ?: xml}"
 }
