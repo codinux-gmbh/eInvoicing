1c95a8158f83cafb9407abbfa3302a1b89e401f1
dankito <git@dankito.net>
2024-12-28 21:10:49 +0100

    Bumped version to 0.6.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 
 allprojects {
     group = "net.codinux.invoicing"
-    version = "0.6.0"
+    version = "0.6.1-SNAPSHOT"
 
     ext["sourceCodeRepositoryBaseUrl"] = "git.dankito.net/codinux/eInvoicing"
 
