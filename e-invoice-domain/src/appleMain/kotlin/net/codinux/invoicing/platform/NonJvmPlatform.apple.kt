e5e13f4977158ddaf23776d374a8c633edb179b1
dankito <git@dankito.net>
2025-01-08 00:54:48 +0100

    Fixed code for Apple systems

--- e-invoice-domain/src/appleMain/kotlin/net/codinux/invoicing/platform/NonJvmPlatform.apple.kt
+++ e-invoice-domain/src/appleMain/kotlin/net/codinux/invoicing/platform/NonJvmPlatform.apple.kt
@@ -1,5 +1,6 @@
 package net.codinux.invoicing.platform
 
+import kotlinx.cinterop.UnsafeNumber
 import net.codinux.invoicing.model.Instant
 import platform.Foundation.*
 import net.codinux.invoicing.model.LocalDate
@@ -9,19 +10,25 @@ internal actual object NonJvmPlatform {
     actual fun getInstantNow(): Instant {
         val secondsSinceEpoch = NSDate().timeIntervalSince1970
 
-        return Instant(secondsSinceEpoch, 0)
+        val nanosString = secondsSinceEpoch.toString().substringAfter('.').let {
+            if (it.length > 9) it.substring(0, 9)
+            else it.padEnd(9, '0')
+        }
+
+        return Instant(secondsSinceEpoch.toLong(), nanosString.toInt())
     }
 
+    @OptIn(UnsafeNumber::class)
     actual fun getLocalDateNow(): LocalDate {
         val currentDate = NSDate()
 
         val calendar = NSCalendar.currentCalendar
         val components = calendar.components(
-            NSCalendar.UnitYear or NSCalendar.UnitMonth or NSCalendar.UnitDay,
+            NSCalendarUnitYear or NSCalendarUnitMonth or NSCalendarUnitDay,
             fromDate = currentDate
         )
 
-        return LocalDate(components.year, components.month, components.day)
+        return LocalDate(components.year.toInt(), components.month.toInt(), components.day.toInt())
     }
 
 }
