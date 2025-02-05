aba644264f3b3568dfa5cfdc73e0f4f550e840c1
dankito <git@dankito.net>
2025-02-05 20:27:34 +0100

    Merged InputStreamExtensions and StreamExtensions

--- e-invoice-domain/src/javaCommonMain/kotlin/net/codinux/invoicing/extension/StreamExtensions.kt
+++ e-invoice-domain/src/javaCommonMain/kotlin/net/codinux/invoicing/extension/StreamExtensions.kt
@@ -1,6 +1,15 @@
 package net.codinux.invoicing.extension
 
 import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+/**
+ * InputStream.readAllBytes() is not available on Android < API 33, so i extracted this method
+ */
+fun InputStream.readAllBytesAndClose(): ByteArray =
+    this.use {
+        this.readBytes()
+    }
 
 fun ByteArrayOutputStream.useAndGet(block: (ByteArrayOutputStream) -> Unit): ByteArray = this.use {
     block(this)
