34afcce952c85af3fb5689d2b3a21303334b7b78
dankito <git@dankito.net>
2025-02-05 17:07:33 +0100

    Categorizing WebClientExceptions

--- e-invoice-domain/src/commonMain/kotlin/net/codinux/invoicing/web/WebClientException.kt
+++ e-invoice-domain/src/commonMain/kotlin/net/codinux/invoicing/web/WebClientException.kt
@@ -1,5 +1,8 @@
 package net.codinux.invoicing.web
 
+import io.ktor.client.network.sockets.*
+import kotlinx.io.IOException
+
 open class WebClientException(
     open val httpStatusCode: Int,
     open val headers: Map<String, List<String>>,
@@ -7,8 +10,21 @@ open class WebClientException(
     cause: Throwable? = null
 ) : Exception(errorMessage, cause) {
 
+    val isConnectTimeout = cause is ConnectTimeoutException
+
+    val isSocketTimeout = cause is SocketTimeoutException
+
+    val isIOException = cause is IOException
+
+    val isNetworkError = isConnectTimeout || isSocketTimeout || isIOException
+
+    val isClientError = httpStatusCode in 400..499
+
+    val isServerError = httpStatusCode in 500..599
+
+
     override fun toString(): String {
-        return "$httpStatusCode $message"
+        return "$httpStatusCode $message. isNetworkError = $isNetworkError, isClientError = $isClientError, isServerError = $isServerError"
     }
 
 }
