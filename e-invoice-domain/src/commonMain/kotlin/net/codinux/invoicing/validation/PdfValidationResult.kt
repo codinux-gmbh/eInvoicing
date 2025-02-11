898f10a26f31133eb2504ab3d9c88351706f8b9a
dankito <git@dankito.net>
2025-02-11 02:36:15 +0100

    Removed default values as all of these values are known / stated now (there's no read error anymore in this class in which case all other properties would be unknown)

--- e-invoice-domain/src/commonMain/kotlin/net/codinux/invoicing/validation/PdfValidationResult.kt
+++ e-invoice-domain/src/commonMain/kotlin/net/codinux/invoicing/validation/PdfValidationResult.kt
@@ -1,16 +1,15 @@
 package net.codinux.invoicing.validation
 
 import kotlinx.serialization.Serializable
-import net.codinux.invoicing.model.dto.SerializableException
 
 @Serializable
 data class PdfValidationResult(
     val isValid: Boolean,
-    val isPdfA: Boolean? = null,
-    val isPdfA3: Boolean? = null,
-    val pdfAFlavor: PdfAFlavour = PdfAFlavour.Unknown,
-    val countExecutedTests: Int = 0,
-    val validationErrors: List<PdfValidationError> = emptyList()
+    val isPdfA: Boolean,
+    val isPdfA3: Boolean,
+    val pdfAFlavor: PdfAFlavour,
+    val countExecutedTests: Int,
+    val validationErrors: List<PdfValidationError>
 ) {
     override fun toString() = "isValid? $isValid: ${validationErrors.joinToString()}"
 }
